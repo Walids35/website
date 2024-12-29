1f83ac1aaa314dd82a303fbb815b0d0fe1dd2415
Walid Siala <wsiala4@gmail.com>
2024-12-29 12:32:25 +0100

    age change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
               hi walid here &#128527;
             </h1>
             <div className="max-w-lg leading-5">
-              21-year-old software engineer from Tunisia TN <br></br> <br></br>{" "}
+              22-year-old software engineer from Tunisia TN <br></br> <br></br>{" "}
               <p className="text-justify">A dedicated software engineering student with expertise in full-stack development, cloud technologies, and innovative solutions</p>
             </div>
             <div className="flex gap-2 items-center">
